ac83b785c9dc951e0d1dd5e46f9c34205711460b
Rtw915 <51250006+Rtw915@users.noreply.github.com>
2024-06-26 15:34:39 -0400

    Create Get-SystemCreatedDefaults.ps1

---
+++ 1.0.0/Private/Get-SystemCreatedDefaults.ps1
@@ -0,0 +1,30 @@
+function Get-SystemCreatedDefaults {
+        <#
+        .SYNOPSIS
+            Gets Default variables, modules, Snapins, and functions
+        .DESCRIPTION
+            This function gets the System-Created default variables, modules, Snapins, and functions. This is needed to be able to filter against to get user defined items.
+        .NOTES
+            Name: Get-SystemCreatedItems
+            Author: Ryan Whitlock
+            Date: 01.11.2024
+            Version: 1.0
+    #>
+    [CmdletBinding()]
+    param()
+    process {
+        # Create a clean PowerShell instance to load system-created default items
+        $StandardUserEnv = [powershell]::Create().addscript({
+            Function _temp {[cmdletbinding(SupportsShouldProcess=$True)] param() }
+            [PSCustomObject]@{                    
+                Modules     = Get-Module | Select-Object -ExpandProperty Name
+                Snapins     = Get-PSSnapin | Select-Object -ExpandProperty Name
+                Functions   = Get-ChildItem function:\ | Select-Object -ExpandProperty Name
+                Variables   = @((Get-Variable | Select-Object -ExpandProperty Name) + (Get-Command _temp | Select-Object -ExpandProperty parameters).Keys)
+            }
+        },$true).invoke()[0]
+    }
+    end {
+        Write-Output $StandardUserEnv
+    }
+}
